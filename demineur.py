6bfecbcdc3cd835c547b582f2e3dea9a7a64bcce
Evan <evanaguttesrivelon@gmail.com>
2025-04-01 12:17:48 +0200

    demineur en ligne de commande finit

--- demineur.py
+++ demineur.py
@@ -39,20 +39,65 @@ def case(x, y):
     else:
         print("Out of the grid! Play again")
 
+def hasLost(x, y, bomb_char):
+	if grid[x][y] == bomb_char:
+		return True
+	return False
+
+def hasWon(empty_char):
+	i = 0
+	for i in range(len(grid)):
+		j = 0
+		for j in range(len(grid[i])):
+			if playerGrid[i][j] == "-":
+				if grid[i][j] == empty_char:
+					return (False)
+			j += 1
+		i += 1
+	return (True)
+
+def count_mines(grid, x, y, bomb_char):
+    # Directions for 8 neighbors
+    directions = [(-1, -1), (-1, 0), (-1, 1), 
+                  (0, -1),           (0, 1), 
+                  (1, -1), (1, 0), (1, 1)]
+    
+    mine_count = 0
+    
+    for dr, dc in directions:
+        neighbor_x = x + dr
+        neighbor_y = y + dc
+        
+        # Check if the neighbor is within bounds of the grid
+        if 0 <= neighbor_x < len(grid) and 0 <= neighbor_y < len(grid[0]):
+            if grid[neighbor_x][neighbor_y] == bomb_char:  # Mine found
+                mine_count += 1
+    return mine_count
 
 if __name__ == "__main__":
-	columns = 10
-	lines = 10
+	columns = int(input("Enter column number for the game: "))
+	lines = int(input("Enter line number for the game: "))
+	bombs = int(input("Enter bomb number for the game: "))
 	grid = gridGenerator(columns, lines, "0")
 	playerGrid = gridGenerator(columns, lines, "-")
 
-	grid = addBombs(grid, 6, "1")
+	grid = addBombs(grid, bombs, "1")
 
+	print("grid generated")
 	printGrid(playerGrid)
 	while True:
 		print("Enter coordinates to open a case")
-		coordinateX = input("Enter lign between 1 to "+ str(columns) + " : ")
-		coordinateY = input("Enter column between 1 to "+ str(lines) + " : ")
+		coordinateX = int(input("Enter lign between 1 to "+ str(columns) + " : "))
+		coordinateY = int(input("Enter column between 1 to "+ str(lines) + " : "))
 
-		case(int(coordinateX)-1, int(coordinateY)-1)
+		case(coordinateX - 1, coordinateY - 1)
 		printGrid(playerGrid)
+		if (hasLost(coordinateX - 1, coordinateY - 1, "1")):
+			print("Sorry, you lost")
+			break
+		elif (hasWon("0")):
+			print("Congrats, You Won")
+			break
+		else:
+			playerGrid[coordinateX - 1][coordinateY - 1] = str(count_mines(grid, coordinateX - 1, coordinateY - 1, "1"))
+		
